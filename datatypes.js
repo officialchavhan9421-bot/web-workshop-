88ce8b15ebf92ebd94e8ddd74740e4d7705ec11a
piyush chavhan <officialchavhan9421@gmail.com>
2026-03-28 21:09:08 +0530

    datatypes

--- datatypes.js
+++ datatypes.js
@@ -5,5 +5,26 @@
 
 // this will not work in vscode because it is a part of browser and it does not work in vscode but it works in browser so we can see the output in browser console.
 
-console.log(3+3);
+console.log(3
+    
+     +3);
+    //   code readibility is not good because it is not properly formatted and it is not easy to read and understand the code. so it is better to format the code properly and make it more readable and understandable.
+
+
 console.log("Hello World");
+
+
+let name = "Piyush Chavhan"
+let age = 22
+let isStudent = true
+let state = null
+
+// name is a string datatype because it is a sequence of characters and it is enclosed in double quotes. 
+// age is a number datatype because it is a numeric value and it is not enclosed in double quotes. 
+// isStudent is a boolean datatype because it can only have two values true or false and it is not enclosed in double quotes.
+// null is a special datatype which represents the absence of any value and it is not enclosed in double quotes. it is used to represent the absence of any value or object. 
+
+console.log(typeof name); //string
+console.log(typeof age); //number
+console.log(typeof isStudent); //boolean
+console.log(typeof state); //object
